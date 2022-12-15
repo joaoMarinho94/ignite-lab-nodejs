9f168e0fd201f974e6d708df508cdf5ba3755c28
joaoMarinho94 <joaopaulim94@gmail.com>
2022-12-15 13:44:03 -0300

    test: improve test send notifications

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,13 +1,24 @@
 import { SendNotification } from './send-notification';
+import { Notification } from '../entities/notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const {} = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recipientId: 'example-recipient-id',
     });
+
+    expect(notifications).toHaveLength(1);
   });
 });
