6edcc91841630fd197b77c1ebf641c4be8a5d194
joaoMarinho94 <joaopaulim94@gmail.com>
2022-12-17 00:25:45 -0300

    feat: create view model to notification controller

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Body, Post } from '@nestjs/common';
 
 import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -17,6 +18,6 @@ export class NotificationsController {
       category,
     });
 
-    return { notification };
+    return { notification: NotificationViewModel.toHTTP(notification) };
   }
 }
