580e63dfe3e46cd5bb8904413ba07ddd970eaa93
joaoMarinho94 <joaopaulim94@gmail.com>
2022-12-17 02:31:30 -0300

    feat: create cancel notification

--- src/application/entities/notification/index.ts
+++ src/application/entities/notification/index.ts
@@ -8,6 +8,7 @@ export interface NotificationsProps {
   category: string;
   readAt?: Date | null;
   createdAt: Date;
+  canceledAt?: Date | null;
 }
 
 export class Notification {
@@ -53,6 +54,14 @@ export class Notification {
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
   }
 
   public get createdAt(): Date {
