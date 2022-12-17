4ce8d48fd7e2bb3f1752f2f2ccf3cf0fd6089d2f
joaoMarinho94 <joaopaulim94@gmail.com>
2022-12-17 02:30:55 -0300

    feat: create findById and save to notifications

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -8,6 +8,12 @@ import { PrismaService } from '../prisma.service';
 @Injectable()
 export class PrismaNotificationRepository implements NotificationsRepository {
   constructor(private prisma: PrismaService) {}
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,6 +4,23 @@ import { NotificationsRepository } from '@application/repositories/notifications
 export class InMemoryNotificationRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    return !notification ? null : notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0)
+      this.notifications[notificationIndex] = notification;
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
